c01cae118c389833d155c33e426f5e529c810f7a
ironrust1 <gol.samen81@gmail.com>
2023-12-25 23:28:14 +0330

    end of day 1 development and i go sleep many works in client and many many works in server side of ftp protocol ...

--- FTP_Client/source.cpp
+++ FTP_Client/source.cpp
@@ -11,6 +11,7 @@ int main(int argc , char **argv)
 {
 	loginStatus = 0;
 	mainMenu();
+	system("pause");
 	return (0);
 }
 
@@ -22,17 +23,19 @@ void mainMenu()
 	char argument[124];
 	WSADATA wsadata;
 	unsigned short serverControlPort = 21; // control connection port (L)
-	unsigned short clientControlPort; // control connection port (U)
+	//unsigned short clientControlPort; //  control connection port (U)
 	unsigned short serverDataPort = 20; // data connection port = control port(L) - 1 
 	unsigned short clientDataPort; //  data connection port = control port (U)
 	int error = WSAStartup(0x0202, &wsadata);
 	if (error){
 		puts("can't initiate network requirements of windows");
+		system("pause");
 		exit(EXIT_FAILURE);
 	}
 	SOCKET clientSock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 	if (clientSock == INVALID_SOCKET){
 		puts("can't create socket ");
+		system("pause");
 		exit(EXIT_FAILURE);
 	}
 	puts("please enter your host ip address for FTP-connection");
@@ -44,12 +47,23 @@ void mainMenu()
 	int connectionStatus = connect(clientSock, (struct sockaddr *) & serverAddr, sizeof(serverAddr));
 	if (connectionStatus == SOCKET_ERROR){
 		puts("can't connect to server please check your network connection and then try again!");
+		system("pause");
 		exit(EXIT_FAILURE);
 	}
+	SOCKET clientDataSock;
+	clientDataSock=socket(AF_INET , SOCK_STREAM , IPPROTO_IP);
+	sockaddr_in clientDataConnectionAddr;
+	clientDataConnectionAddr.sin_family=AF_INET;
+	clientDataConnectionAddr.
+	clientDataConnectionAddr.sin_addr.S_un.S_addr = serverAddr.sin_addr.S_un.S_addr;
+	bind(clientDataSock,
+
 	struct sockaddr_in clientAddr;
 	int clientAddrLen;
-	getsockname(clientSock, (struct sockaddr*) &clientAddr,&clientAddrLen);
-	printf("%hu", ntohs(clientAddr.sin_port));
+	//getsockname(clientSock, (struct sockaddr*) &clientAddr,&clientAddrLen);  // niazi behesh nashod
+	//clientDataPort=ntohs(clientAddr.sin_port);				// niazi behesh peyda nashod
+	//printf("%hu", ntohs(clientAddr.sin_port));  // for debugging
+	int sendStatus;
 	do
 	{
 		puts("please enter your command :");
@@ -58,50 +72,64 @@ void mainMenu()
 		sscanf("%s %s", method, argument);
 		if (strcmp(method, "USER") == 0)
 		{
-
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "PASS") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "PASS") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "LIST") == 0)
 		{
 		}
 		else if (strcmp(method, "RETR") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "STOR") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "DELE") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "MKD") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "RMD") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "PWD") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "CWD") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "CDUP") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "QUIT") == 0) // since here PROjECT NEED
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "HELP") == 0)
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 		else if (strcmp(method, "PORT") == 0) // sets client data port
 		{
+			sendStatus=send(clientSock,command,strlen(command)+1,0);
 		}
 
 	}while (true);
+
 }
